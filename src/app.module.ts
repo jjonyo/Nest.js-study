bd1f184a59a21a1a79a93e55f70325880a361a77
jjonyo <jonghyuk96@cau.ac.kr>
2021-07-27 20:12:50 +0900

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MoviesModule } from './movies/movies.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
